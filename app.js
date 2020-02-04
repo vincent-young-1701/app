e2494e8076a8b38613cf96099ea58c3f7d90e4a2
Vincent Young <vy@Vincents-MacBook-Pro.local>
2020-02-03 17:08:16 -0800

    serving static html page

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
 
-app.get('/', (req, res) => {
-        res.send('Hello');
+app.get("/", (req, res) => {
+  res.removeHeader("X-Powered-By");
+  res.header("X-Website", "weatherwizard.xyz");
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
