57b95f7b7795be19d0128a5cda92c3723e51dfc3
Vincent Young <vy@weatherwizard.xyz>
2020-05-29 21:39:35 -0700

    this is a test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 const path = require("path");
 
 app.get("/", (req, res) => {
+  console.log("test");
   res.removeHeader("X-Powered-By");
   res.header("X-Website", "weatherwizard.xyz");
   res.sendFile(path.join(__dirname + "/index.html"));
